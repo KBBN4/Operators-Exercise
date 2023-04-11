02c873cc369ca4dfa58a1507908fbe276e1d570b
katherine Buendia <kbbuendia24@gmail.com>
2023-04-11 15:49:43 -0400

    operator Excercise

---
+++ OperatorExercise/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+
+namespace OperatorExercise
+{
+	public class Circle
+	{
+        public static double AreaOfCircle(double radius)
+        {
+            double area = Math.PI * radius * radius;
+           
+            return area;
+
+        }
+
+        
+
+    }
+}
+
--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace OperatorExercise
+﻿using System.Data;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          
+            /* x += y;
+             x -= y;
+             x *= y;
+             x /= y;
+             x %= y; */
+
+            int a = 17;
+
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine(a + "/" + b + " is " + b + " remainder " + 1);
+                //Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+            }
+         
+
+
+            // Second Excercise //
+
+
+            Console.WriteLine("What is the radius of your circle?");
+            double input = double.Parse(Console.ReadLine());
+
+            var answer = Circle.AreaOfCircle(input);
+            Console.WriteLine(answer);
+
+
+
+
+            
+
+
+
         }
+
+
     }
 }
